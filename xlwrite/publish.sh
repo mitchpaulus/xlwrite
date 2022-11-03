6de3f23d06e4f4ac118eddc6c587fed9178be2d2
Mitchell Paulus <paulusm14@gmail.com>
2022-11-03 11:20:33 -0500

    Enable null checking, update publish.sh

--- xlwrite/publish.sh
+++ xlwrite/publish.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-dotnet publish -r linux-x64 -o publish/ -c Release -p:DebugType=None --no-self-contained -p:PublishSingleFile=true
+dotnet publish -r linux-x64 -o publish/ -c Release -p:DebugType=None --no-self-contained -p:PublishSingleFile=true && \
+cp publish/xlwrite "$LOCALBIN"/xlwrite
