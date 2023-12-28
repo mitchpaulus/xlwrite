ade90f490709b43a59d2f9c0f62a80bbac786a47
Mitchell Paulus <paulusm14@gmail.com>
2023-12-28 16:30:01 -0600

    Update tests

--- xlwritetests/UnitTest1.cs
+++ xlwritetests/UnitTest1.cs
@@ -3,65 +3,64 @@ using NUnit.Framework;
 using System.Text.RegularExpressions;
 using xlwrite;
 
-namespace Tests
+namespace Tests;
+
+public class Tests
 {
-    public class Tests
+    [Test]
+    public void CellReferenceParser()
     {
-        [Test]
-        public void CellReferenceParser()
-        {
-            string a1Reference = "B23";
-            bool success  = XlWriteUtilities.TryParseCellReference(a1Reference, out Cell cell);
+        string a1Reference = "B23";
+        bool success  = XlWriteUtilities.TryParseCellReference(a1Reference, out Cell? cell);
 
-            Assert.AreEqual(null, cell.SheetName);
-            Assert.AreEqual(-1, cell.SheetNum);
-            Assert.AreEqual(2, cell.Column);
-            Assert.AreEqual(23, cell.Row);
+        Assert.That(cell?.SheetName, Is.EqualTo(null));
+        Assert.That(cell?.SheetNum, Is.EqualTo(-1));
+        Assert.That(cell?.Column, Is.EqualTo(2));
+        Assert.That(cell?.Row, Is.EqualTo(23));
 
-            string r1c1Reference = "r12c65";
-            success = XlWriteUtilities.TryParseCellReference(r1c1Reference, out cell);
-            Assert.AreEqual(65, cell.Column);
-            Assert.AreEqual(12, cell.Row);
-        }
+        string r1c1Reference = "r12c65";
+        success = XlWriteUtilities.TryParseCellReference(r1c1Reference, out cell);
+        Assert.That(cell?.Column, Is.EqualTo(65));
+        Assert.That(cell?.Row, Is.EqualTo(12));
+    }
 
-        [Test]
-        public void NamedWorksheetTest()
-        {
-            string namedWorksheetReference = "'Sheet 1'!B23";
-            bool success  = XlWriteUtilities.TryParseCellReference(namedWorksheetReference, out Cell cell);
-            Assert.AreEqual("Sheet 1", cell.SheetName);
-            Assert.AreEqual(-1, cell.SheetNum);
-            Assert.AreEqual(2, cell.Column);
-            Assert.AreEqual(23, cell.Row);
-        }
+    [Test]
+    public void NamedWorksheetTest()
+    {
+        string namedWorksheetReference = "'Sheet 1'!B23";
+        bool success  = XlWriteUtilities.TryParseCellReference(namedWorksheetReference, out Cell? cell);
+        Assert.That(cell?.SheetName, Is.EqualTo("Sheet 1"));
+        Assert.That(cell?.SheetNum, Is.EqualTo(-1));
+        Assert.That(cell?.Column, Is.EqualTo(2));
+        Assert.That(cell?.Row, Is.EqualTo(23));
+    }
 
-        [Test]
-        public void NumberedSheetTest()
-        {
-            string numberSheetReference = "1!B23";
-            bool success  = XlWriteUtilities.TryParseCellReference(numberSheetReference, out Cell cell);
-            Assert.AreEqual(null, cell.SheetName);
-            Assert.AreEqual(1, cell.SheetNum);
-            Assert.AreEqual(2, cell.Column);
-            Assert.AreEqual(23, cell.Row);
-        }
+    [Test]
+    public void NumberedSheetTest()
+    {
+        string numberSheetReference = "1!B23";
+        bool success  = XlWriteUtilities.TryParseCellReference(numberSheetReference, out Cell? cell);
+        Assert.That(cell?.SheetName, Is.EqualTo(null));
+        Assert.That(cell?.SheetNum, Is.EqualTo(1));
+        Assert.That(cell?.Column, Is.EqualTo(2));
+        Assert.That(cell?.Row, Is.EqualTo(23));
+    }
 
-        [Test]
-        public void RegexTests()
-        {
-            string test = "]";
-            Regex regex = new(@"[\]]");
-            Assert.IsTrue(regex.Match(test).Success);
-        }
+    [Test]
+    public void RegexTests()
+    {
+        string test = "]";
+        Regex regex = new(@"[\]]");
+        Assert.That(regex.Match(test).Success);
+    }
 
-        [Test]
-        public void DateParseTest()
-        {
-            string test = "1/6";
-            bool success = DateTime.TryParse(test, out DateTime dateTime);
-            // Print ISO 8601 date format
-            Console.WriteLine(dateTime.ToString("yyyy-MM-dd"));
-            Assert.IsTrue(success);
-        }
+    [Test]
+    public void DateParseTest()
+    {
+        string test = "1/6";
+        bool success = DateTime.TryParse(test, out DateTime dateTime);
+        // Print ISO 8601 date format
+        Console.WriteLine(dateTime.ToString("yyyy-MM-dd"));
+        Assert.That(success);
     }
 }
