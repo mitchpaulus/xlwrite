a1c9176426548bb9f3c7a66ab454a359c6edc8ea
Mitchell Paulus <paulusm14@gmail.com>
2024-12-16 13:26:52 -0600

    Add AssemblyInfo to get rid of ANTLR warnings

---
+++ xlwrite/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+// This is to remove Antlr compiler warnings.
+// References:
+//https://github.com/tunnelvisionlabs/antlr4cs/issues/10#issuecomment-66999851
+using System;
+[assembly: CLSCompliant(false)]
